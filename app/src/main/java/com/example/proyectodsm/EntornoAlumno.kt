bb7adf42c8071a5863240c05b75bcf86e848f490
Kath001 <katherinem.alfarorico@outlook.com>
2023-04-25 17:44:52 -0600

    git push

---
+++ app/src/main/java/com/example/proyectodsm/EntornoAlumno.kt
@@ -0,0 +1,28 @@
+package com.example.proyectodsm
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class EntornoAlumno : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_entorno_alumno, container, false)
+    }
+}
+
+
+
